b95c977c4cabd5ba1d5a7e39b6bfcae2cbc6d750
Andrew Choi <a.choi@smartsoltech.kr>
2023-07-18 23:27:28 +0900

    bugfix & features added

--- Seminar1/Seminar_2/HW1.py
+++ Seminar1/Seminar_2/HW1.py
@@ -1,6 +1,6 @@
 from fractions import Fraction
 
-def integer_to_hex(num):
+def integer_to_hex(number: int):
     """
     Преобразует целое число в его шестнадцатеричное строковое представление.
 
@@ -10,9 +10,9 @@ def integer_to_hex(num):
     Возвращает:
     - str: Шестнадцатеричное представление целого числа.
     """
-    return hex(num)[2:]
+    return hex(number)[3:]
 
-def calculate_fraction(input_string):
+def calculate_fraction(input_string: str):
     """
     Вычисляет дробь на основе входной строки в формате "a/b".
 
@@ -22,6 +22,7 @@ def calculate_fraction(input_string):
     Возвращает:
     - Fraction: Результат дроби, представленный в виде объекта Fraction из модуля fractions.
     """
+# Разбор строк на числитель и знаменатель
     numerator, denominator = map(int, input_string.split('/'))
     fraction = Fraction(numerator, denominator)
     return fraction
@@ -35,7 +36,7 @@ def program_1():
     print("Шестнадцатеричное представление:", hex_string)
     print("Проверка:", hex(num))
 
-    expected_hex_string = hex(num)[2:]
+    expected_hex_string = '0x' + hex(num)[2:]
     if hex_string == expected_hex_string:
         print("Результат верный.")
     else:
@@ -50,13 +51,12 @@ def program_2():
 
     sum_fraction = calculate_fraction(fraction1) + calculate_fraction(fraction2)
     product_fraction = calculate_fraction(fraction1) * calculate_fraction(fraction2)
-
+    
     print("Сумма дробей:", sum_fraction)
     print("Произведение дробей:", product_fraction)
 
     expected_sum_fraction = Fraction(fraction1) + Fraction(fraction2)
     expected_product_fraction = Fraction(fraction1) * Fraction(fraction2)
-
     if sum_fraction == expected_sum_fraction and product_fraction == expected_product_fraction:
         print("Результат верный.")
     else:
@@ -65,9 +65,6 @@ def program_2():
         print("Ожидаемое произведение:", expected_product_fraction)
 
 def main():
-    """
-    Главная функция программы.
-    """
     while True:
         print("Меню:")
         print("1 - Программа 1: Шестнадцатеричное представление целого числа")
@@ -75,12 +72,16 @@ def main():
         print("0 - Выход")
 
         choice = input("Выберите программу (введите номер): ")
-        if choice == "1":
-            program_1()
-        elif choice == "2":
-            program_2()
-        elif choice == "0":
-            break
+        if choice.isdigit():
+            choice = int(choice)
+            if choice == 1:
+                program_1()
+            elif choice == 2:
+                program_2()
+            elif choice == 0:
+                break
+            else:
+                print("Неверный выбор. Попробуйте снова.")
         else:
             print("Неверный выбор. Попробуйте снова.")
 
