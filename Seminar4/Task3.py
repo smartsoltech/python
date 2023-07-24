b5f37c7a9669bdda6db8c0aab082bcf49328c39e
Andrew Choi <a.choi@smartsoltech.kr>
2023-07-25 03:21:17 +0900

    some fixes

--- Seminar4/Task3.py
+++ Seminar4/Task3.py
@@ -1,6 +1,14 @@
-def create_unicode_dict(numbers_string):
+def create_unicode_dict(numbers_string:str) -> dict[int, str]:
+    """_summary_
+
+    Args:
+        numbers_string (str): _description_
+
+    Returns:
+        dict[int, str]: _description_
+    """
     num1, num2 = map(int, numbers_string.split())
     start, end = min(num1, num2), max(num1, num2)
     return {chr(i): i for i in range(start, end + 1)}
 
-print(create_unicode_dict("65 70"))
+print(create_unicode_dict("65 90"))
--- Seminar4/Task5.py
+++ Seminar4/Task5.py
@@ -1,12 +1,22 @@
-def calculate_bonus(names, rates, bonuses):
+def calculate_bonus(names: list, rates: list, bonuses: list) -> dict[str, int]:
+    """Вычисление бонусов
+
+    Args:
+        names (list): Список получаталей бонуса
+        rates (list): Оплата
+        bonuses (list): Размер бонуса от Оплаты
+
+    Returns:
+        dict[str, int]: Список получателей бонуса с суммой бонуса
+    """
     bonus_dict = {}
     for name, rate, bonus in zip(names, rates, bonuses):
         bonus_percent = float(bonus.strip('%')) / 100
         bonus_dict[name] = rate * bonus_percent
     return bonus_dict
 
-names = ["John", "Lisa", "Robert"]
+names = ["Жека", "Васька", "Петька"]
 rates = [1000, 1500, 1200]
-bonuses = ["10%", "15%", "20%"]
+bonuses = ["56%", "12.5345%", "7%"]
 
 print(calculate_bonus(names, rates, bonuses))
