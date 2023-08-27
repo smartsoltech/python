253d3cc10e3d70c09173955d0bb667861db2721f
Andrew Choi <a.choi@smartsoltech.kr>
2023-08-27 21:04:17 +0900

    Code comments added

--- Seminar9/quadratic_solver.py
+++ Seminar9/quadratic_solver.py
@@ -4,7 +4,14 @@ import json
 import math
 import random
 
+# Константы
+CSV_FILENAME = "coefficients.csv"
+JSON_FILENAME = "results.json"
+MIN_COEFFICIENT = 1
+MAX_COEFFICIENT = 10
+
 def load_equation_values_decorator(func):
+    """Декоратор для загрузки значений уравнения из CSV файла"""
     def wrapper(filename, args):
         with open(filename, 'r') as csvfile:
             reader = csv.reader(csvfile)
@@ -19,27 +26,30 @@ def load_equation_values_decorator(func):
     return wrapper
 
 def save_to_json(a, b, c, result):
+    """Сохраняет параметры и результаты работы функции в JSON файл"""
     data = {
         "a": a,
         "b": b,
         "c": c,
         "result": result
     }
-    with open("results.json", "a") as jsonfile:
+    with open(JSON_FILENAME, "a") as jsonfile:
         json.dump(data, jsonfile)
         jsonfile.write('\n')
 
 def generate_csv(filename, rows):
+    """Генерирует CSV файл с случайными коэффициентами"""
     with open(filename, 'w', newline='') as csvfile:
         csv_writer = csv.writer(csvfile)
         for _ in range(rows):
-            a = random.randint(1, 10)
-            b = random.randint(1, 10)
-            c = random.randint(1, 10)
+            a = random.randint(MIN_COEFFICIENT, MAX_COEFFICIENT)
+            b = random.randint(MIN_COEFFICIENT, MAX_COEFFICIENT)
+            c = random.randint(MIN_COEFFICIENT, MAX_COEFFICIENT)
             csv_writer.writerow([a, b, c])
         print(f"Сгенерирован файл '{filename}' с {rows} строками.")
 
 def quadratic_roots(a, b, c):
+    """Находит корни квадратного уравнения"""
     discriminant = b ** 2 - 4 * a * c
     if discriminant > 0:
         x1 = (-b + math.sqrt(discriminant)) / (2 * a)
